e0135ce5ab5ff306d52c8b9545a9e09e5b043b19
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-01-18 22:26:26 +0000

    Created login page

--- src/app/dashboard/(admin)/login/page.tsx
+++ src/app/dashboard/(admin)/login/page.tsx
@@ -1,25 +1,49 @@
 "use client";
 
-import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { FC, FormEvent, useState } from "react";
+import instance from "src/app/_lib/axiosBase";
 
 const Login: FC = () => {
+    const router = useRouter();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const handleLogin = async (e: FormEvent) => {
         e.preventDefault();
+        
+        const credentials = {
+            UserName: username,
+            Password: password
+        }
 
-        const response = await axios.post("https://kxlsxconverterapi.onrender.com/User")
+        try {
+            const response = await instance.post("/User/SignIn", credentials, { withCredentials: true });
+            console.log(response.data);
+            router.push("/dashboard/employees");
+        } catch (error) {
+            console.error(error);
+            clearForm();
+        }
+    }
+
+    const clearForm = () => {
+        setUsername("");
+        setPassword("");
     }
     
     return (
         <div id="login-page">
-            <form id="id-form" onSubmit={(e) => handleLogin(e)}>
+            <Link href={"/"}>
+                <button>Return to Homepage</button>
+            </Link>
+            <form id="login-form" onSubmit={(e) => handleLogin(e)}>
                 <label htmlFor="username">Username: </label>
                 <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 <label htmlFor="password">Password: </label>
                 <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Login</button>
             </form>
         </div>
     )
