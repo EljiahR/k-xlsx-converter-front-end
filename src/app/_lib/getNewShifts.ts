dbb83122401a579c7237d5d81a3df90370f58af9
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-02-09 16:23:04 +0000

    Updated getNewShifts to typescript

---
+++ src/app/_lib/getNewShifts.ts
@@ -0,0 +1,25 @@
+import instance from "./axiosBase.js";
+import formatWeek from "./dtoToBO.js";
+
+export const getEmployees = async (xlsxFile) => {
+  const data = new FormData();
+  data.append("file", xlsxFile);
+
+  const response = await instance.post("/Employee/Dailies/16/549", data, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
+
+  if (response.status != 200) {
+    const error = await response.statusText;
+    console.error("Error response:", error);
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const newShifts = response.data;
+  console.log(newShifts);
+  const formattedShifts = formatWeek(newShifts);
+
+  return formattedShifts;
+};
