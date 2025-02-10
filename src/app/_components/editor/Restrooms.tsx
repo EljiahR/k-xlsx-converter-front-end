e241ee976ac3a3dfdd0e59f19325adc3653e84a6
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-02-10 23:36:42 +0000

    Refractored Restrooms to reduce repetition, and added in the first found utility clerk to each time slot

--- src/app/_components/editor/Restrooms.tsx
+++ src/app/_components/editor/Restrooms.tsx
@@ -1,5 +1,7 @@
 import styles from "@/styles/Restrooms.module.css";
 import { IEmployeeBO } from "src/app/_lib/dtoToBO";
+import { utilityTimes } from "src/app/_lib/lotTimes";
+import { timeIsWithin } from "src/app/_lib/timeFunctions";
 
 interface Props {
   utilityClerks: IEmployeeBO[]
@@ -16,70 +18,15 @@ const Restrooms = ({utilityClerks}: Props) => {
         <div className={styles.header}>Time</div>
         <div className={styles.header}>Utility Clerk</div>
       </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>11:00 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>11:30 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>12:00 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>12:30 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>1:00 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>1:30 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>2:00 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot} ${styles.dark}`}>
-        <div className={styles.time}>2:30 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>3:00 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot} ${styles.dark}`}>
-        <div className={styles.time}>3:30 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>4:00 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot} ${styles.dark}`}>
-        <div className={styles.time}>4:30 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={styles.slot}>
-        <div className={styles.time}>5:00 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot} ${styles.dark}`}>
-        <div className={styles.time}>5:30 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot}`}>
-        <div className={styles.time}>6:00 PM</div>
-        <div className={styles.blank}></div>
-      </div>
-      <div className={`${styles.slot} ${styles.dark}`}>
-        <div className={styles.time}>6:30 PM</div>
-        <div className={styles.blank}></div>
-      </div>
+      {utilityTimes.map((time) => {
+        return (
+          <div className={`${styles.slot}`}>
+            <div className={styles.time}>{time}</div>
+            <div className={styles.blank}>{utilityClerks.find((clerk) => timeIsWithin(clerk.subshift.shiftStart, clerk.subshift.shiftEnd, time)).firstName}</div>
+          </div>
+        );
+      })}
+      
     </div>
   );
 };
