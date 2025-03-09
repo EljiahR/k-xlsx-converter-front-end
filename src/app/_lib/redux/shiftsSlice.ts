2ca77353e3fe3e7d87c9180b75c7c8a43fa1ca89
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-03-09 15:00:20 +0000

    Added some redux types in store.ts

--- src/app/_lib/redux/shiftsSlice.ts
+++ src/app/_lib/redux/shiftsSlice.ts
@@ -1,17 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IWeekdayBO } from "../types/shiftTypes";
+import { cloneDeep } from "lodash"
+import { expectedOutput } from "../test/expectedOutput";
+
+interface ShiftsState {
+    value: IWeekdayBO[]
+}
+
+const initialState: ShiftsState = {
+    value: null
+};
 
 export const shiftsSlice = createSlice({
     name: "shifts",
-    initialState: {
-        value: 0
-    },
+    initialState,
     reducers: {
-        increment: (state) => {
-            state.value += 1;
+        setAsTest: (state) => {
+            state.value = cloneDeep(expectedOutput);
+        },
+        reset: (state) => {
+            state.value = null;
+        },
+        setNewShifts: (state, action: PayloadAction<IWeekdayBO[]>) => {
+            state.value = action.payload;
         }
     }
 });
 
-export const { increment } = shiftsSlice.actions;
+export const { setAsTest } = shiftsSlice.actions;
 
 export default shiftsSlice.reducer;
--- src/app/_lib/redux/store.ts
+++ src/app/_lib/redux/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import shiftsReducer from "./shiftsSlice"
 
-export default configureStore({
+const store = configureStore({
     reducer: {
         shifts: shiftsReducer
     },
-})
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
