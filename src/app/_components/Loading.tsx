3cf9e393ce1952ebb9372a831d1d4904c4ea9665
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2026-04-15 18:01:37 +0000

    Adding new loading screen

--- src/app/_components/Loading.tsx
+++ src/app/_components/Loading.tsx
@@ -1,6 +1,31 @@
+import styles from "@/styles/Loading.module.css";
+
 const Loading = () => {
- 
-    return <p>Loading...</p>;
+    return 	(
+        <div id={styles["loading-container"]}>
+            <div id={styles["loading-main"]}>
+                <div className={styles["bar-container"]}>
+                    <div className={styles["bar"]}></div>
+                </div>
+                <div className={styles["bar-container"]}>
+                    <div className={styles["bar"]}></div>
+                </div>
+                <div className={styles["bar-container"]}>
+                    <div className={styles["bar"]}></div>
+                </div>
+                <div className={styles["bar-container"]}>
+                    <div className={styles["bar"]}></div>
+                </div>
+                <div className={styles["bar-container"]}>
+                    <div className={styles["bar"]}></div>
+                </div>
+                <div className={styles["bar-container"]}>
+                    <div className={styles["bar"]}></div>
+                </div>
+            </div>
+        </div>
+        
+    );
   
 };
 export default Loading;
